b96af6ec560eff9d1a0259df118d8a2a87e28bed
Saurav Dhyani <66352459+sauravdhyani24@users.noreply.github.com>
2024-02-03 15:02:51 +0000

    ok

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,2 +1,16 @@
-let score = 33
-console.log(typeof score)
+let score = null
+//console.log(typeof score)
+
+let num = Number(score)
+//console.log(typeof num)
+//console.log(num)
+
+//"33" => 33
+// "33abc" => NaN
+// True => 1 ; False=> 0
+// undefined => NaN , Null => 0
+
+let isloggedin = 0
+
+let booleanisloggedin = Boolean(isloggedin)
+console.log(booleanisloggedin)
