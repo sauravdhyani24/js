1f6d0a4f0ebebb53b63051a62eb7b9920f3b7975
Saurav Dhyani <66352459+sauravdhyani24@users.noreply.github.com>
2024-02-03 15:45:24 +0000

    changes in files

---
+++ 01_basics/04_datatypes.js
@@ -0,0 +1,27 @@
+// pimitive datatypes
+// 7 types: string, Number, Boolean, null, undefined, symbol, BigInt
+const score =33
+const isloggedin = false
+const scorevalue = 33.5
+const temp = null
+const infnum = undefined
+
+const id = Symbol("123")
+const userid = Symbol("123")
+//console.log(id===userid);
+const bignum =23456723456n
+// referance type, nonprimitivev datatypes
+// Arrays, Objects, Functions
+
+const name = ["shubham", "sahil", "rahul"]
+let myobj = {
+    name: "rahul",
+    age: 23,
+    roll: 21
+
+}
+
+function myfunction()
+{
+    console.log("hello world")
+}
